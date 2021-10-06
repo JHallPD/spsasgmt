526ffe92b7a532245251b1a3249abfd8187b7238
Jeff <jeffahall94@gmail.com>
2021-10-06 14:51:48 -0400

    moving to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import logo from './logo.svg';
+import Card from "./card";
+import {Header} from "antd/es/layout/layout";
 
 function App() {
+    /// mapping is having issues with using a key or index seemingly so I have added it as data for now
+    const [list, setList] = useState([{
+        key:0,
+        bodyColor:"#4F4F4F",
+        titleColor: "#0E2748",
+        titleSize: "36px",
+        bodySize: "16px",
+        title: "Custom Title",
+        text: "Custom body text",}]);
+
+    const deleteCard = (key) => {
+        setList(list.filter(item => item.key !== key));
+    }
+    const copyCard = (data) => {
+        setList([...list, {...data, key:list.length}]);
+    };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Header>
+        <div className="App-body">
+            {list.map((data, key) => <Card key={key} data={data} lastCard={list.length === 1} copyCard={copyCard} deleteCard={deleteCard}/>)}
+        </div>
     </div>
   );
 }
---
+++ src/card.js
@@ -0,0 +1,134 @@
+import React from "react";
+import './App.css';
+import edit from "./edit.svg";
+import copy from './copy.svg';
+import deleteW from './deleteW.svg';
+import deleteB from './deleteB.svg';
+import settings from './Settings.svg';
+import style from './Style.svg';
+import settingsS from './Settings (1).svg';
+import styleS from './Style (1).svg';
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            tab: "settings",
+            editable: false,
+            ...this.props.data
+        };
+    }
+    // copy card
+    copy = () => {
+        this.props.copyCard(this.props.data)
+    }
+    // delete card
+    delete = () => {
+        this.props.deleteCard(this.props.data.key)
+    }
+    // open edit modal
+    openEdit = () => {
+        let oldEL = document.getElementsByClassName("card-div-edit")[0]
+        let el = document.getElementById(this.props.data.key)
+
+        if(oldEL){
+            oldEL.classList.add("card-div")
+            oldEL.classList.remove("card-div-edit")
+        }
+        if(oldEL !== el){
+            el.classList.add("card-div-edit")
+            el.classList.remove("card-div")
+        }
+
+        this.setState({ editable: !this.state.editable })
+        // open modal for color & text editor
+        // color & text editor should edit the props
+    }
+    changeTab = (e) =>{
+        console.log(e.target.name)
+
+        this.setState({ tab: e.target.name })
+    }
+    edit = (e) =>{
+        console.log(e)
+        switch (e.target.name) {
+            case "title":
+                this.setState({ title: e.target.value })
+                break
+            case "body":
+                this.props.data.text = e.target.value
+                break
+            case "titleColor":
+                this.props.data.titleColor = e.target.value
+                break
+            case "titleSize":
+                this.props.data.titleSize = e.target.value
+                break
+            case "bodyColor":
+                this.props.data.bodyColor = e.target.value
+                break
+            case "bodySize":
+                this.props.data.bodySize = e.target.value
+                break
+            default:
+                console.log("shouldnt be here :/")
+        }
+    }
+    render() {
+        return <div className="card-div" style={{order: this.state.key}} id={this.state.key}>
+            <div className="card-title-box">
+                <p className="card-title" style={{color: this.state.titleColor, fontSize:this.state.titleSize}}>{this.state.title}</p>
+                <img src={edit} className="card-edit" alt="edit" onClick={this.openEdit}/>
+                <img src={copy} className="card-copy" alt="copy" onClick={this.copy}/>
+                {this.props.lastCard?
+                    <img src={deleteW} className="card-delete-blank" alt="delete" onClick={null}/>
+                    :
+                    <img src={deleteB} className="card-delete" alt="delete" onClick={this.delete}/>}
+            </div>
+            <div className="card-body" >
+                <p className="card-text" style={{color: this.props.data.bodyColor, fontSize:this.props.data.bodySize}}>{this.props.data.text}</p>
+            </div>
+            {this.state.editable && (                <div className="editModal" id={"edit-"+this.props.data.key} >
+                    <div className="tabs">
+                        <div className="edit-settings" name="settings" id={"edit-settings-" + this.props.data.key}onClick={() => this.changeTab}>
+                            {this.state.tab === "settings"?
+                                <img src={settings} className="selected"  alt="settings"/>
+                                :<img src={settingsS}  alt="settings"/>
+                            }
+                        </div>
+                        <div className="edit-style" name="style" id={"edit-style-" + this.props.data.key} onClick={() => this.changeTab}>
+                            {this.state.tab === "settings"?
+                                <img src={style} alt="style"/>
+                                :<img src={styleS} className="selected"  alt="style"/>}
+
+                        </div>
+                    </div>
+                    {this.state.tab === "settings"?
+                        <div className="content">
+                            <div className="title-box">
+                                <label className="title">
+                                    <p className="title-text">Title Text</p>
+                                    <input type="text" name="title" className="title-input" onChange={this.edit} placeholder={"Enter custom title"}/>
+                                </label>
+                            </div>
+                            <div className="text-box">
+                                <label className="text">
+                                    <p className='text-text'>Body Text</p>
+                                    <input type="text" name="body" className="text-input" onChange={this.edit} placeholder={"Enter custom text"}/>
+                                </label>
+                            </div>
+
+                        </div>:
+                        <div className="content">
+
+                        </div>
+                    }
+
+                </div>
+            )}
+        </div>
+    }
+}
+
+
+export default Card;
